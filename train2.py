780a5b56221797adcd1c63c7ed91185e058bbfa7
Amish Kar <amish.kar08@gmail.com>
2025-10-12 18:07:44 +0530

    viva

--- train2.py
+++ train2.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
-from misc import load_data, preprocess
+from misc import load_data, preprocess, train_model, evaluate_model
 
 def main():
     df = load_data()
@@ -23,10 +23,14 @@ def main():
 
     cv = KFold(n_splits=5, shuffle=True, random_state=42)
     gs = GridSearchCV(pipe, param_grid, scoring="neg_mean_squared_error", cv=cv, n_jobs=-1)
-    gs.fit(X_train, y_train)
 
-    y_pred = gs.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
+   
+    #gs.fit(X_train, y_train)
+    #y_pred = gs.predict(X_test)
+    #mse = mean_squared_error(y_test, y_pred)
+    
+    kr = train_model(gs, X_train, y_train)
+    mse = evaluate_model(kr, X_test, y_test)
     print("Best params:", gs.best_params_)
     print(f"Kernel Ridge (RBF) Test MSE: {mse:.4f}")
 
